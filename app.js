8142a546af633adcae03bc43efb2a7347725d122
mmarquiscreative <mmarquis.creative@gmail.com>
2017-12-18 17:02:05 -0800

    build 12.18

--- app.js
+++ app.js
@@ -9,54 +9,146 @@
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
+var calendarController = (function(){
+ 
+//// VARIABLES ////
+    var mealSchedule = [];
+    
+    
+    
+    
+//// FUNCTION ////
+    function generateMonth(howManyWks, recipesObj){
+        var recipes = recipesObj;
+        var weeks = [];
+        console.log(howManyWks);
+        // console.log(Object.values(recipes.breakfast));
+        //1. Gen weeks for howManyWks
+        for(var i = 0; i < 4; i++){                
+            //1. Gen Breakfast
+        var breakfastObj =  recipes.breakfast;
+        var breakfastArray = genWeek(breakfastObj);
+                    
+            //2. Gen Lunches
+        var lunchObj = recipes.lunch;
+        var lunchArray = genWeek(lunchObj);
+                    
+            //3. Gen dinners
+        var dinnerObj = recipes.dinner;
+        var dinnerArray = genWeek(dinnerObj);
+            
+            weeks.push([breakfastArray, lunchArray, dinnerArray]);
+        };
+        //2. push schedule to mealSchedule
+        mealSchedule = weeks;
+        console.log(mealSchedule);
+        
+        return weeks;
+    };
+    
+    function genWeek(someObj){
+        var weekArray = [];
+                for(var i = 0; i < 7; i++){
+                    
+                   var tempRandom = (Math.round(Math.random()) * (someObj.length - 1))
+                    weekArray.push(someObj[tempRandom]);
+                   };
+        return weekArray;
+    };
+    
+return {
+    generateMonth: generateMonth
+}    
+})();
+
 
 var dataController = (function(){
     
     //// VARIABLES ////
     var recipes = {
-        latkes: [
+        breakfast: [
+            ['latkes',
                 [4, ' lbs ', 'Potato'],
                 [1, ' medium ', 'Onion'],
                 [4, ' ', 'Egg'],
                 [1, ' Tbsp ', 'Garlic']
             ],
-        oatmeal: [
+            ['Oatmeal',
                 [1.5, ' Cup ', 'Oats'],
                 [3, ' Cup ', 'Water'],
                 [0.5, ' Cup ', 'Strawberry']
+            ]
+        ],
+        lunch: [
+            ['pollo',
+                [4, ' lbs ', 'Potato'],
+                [1, ' medium ', 'Onion'],
+                [4, ' ', 'Egg'],
+                [1, ' Tbsp ', 'Garlic']
+            ],
+            ['soup',
+                [1.5, ' Cup ', 'Oats'],
+                [3, ' Cup ', 'Water'],
+                [0.5, ' Cup ', 'Strawberry']
+            ]
+        ],
+        dinner: [
+            ['pasta',
+                [4, ' lbs ', 'Potato'],
+                [1, ' medium ', 'Onion'],
+                [4, ' ', 'Egg'],
+                [1, ' Tbsp ', 'Garlic']
+            ],
+            ['Moose',
+                [1.5, ' Cup ', 'Oats'],
+                [3, ' Cup ', 'Water'],
+                [0.5, ' Cup ', 'Strawberry']
+            ]
         ]
+        
     };
     
-    var recipeQueue = [recipes.latkes, recipes.oatmeal, recipes.latkes];
+    var recipeQueue = [recipes.breakfast[0], recipes.breakfast[1], recipes.breakfast[0]];
     
-    var allIngredients = [];
+   
     
-    var compiledIngredients = [];
     
-    var concatIngredList = [];
     
     //// FUNCTIONS ////
-    function loadIngredients(){
-        recipeQueue.forEach(function(cur){
-            cur.forEach(function(cur){
-                allIngredients.push(cur);
-        
-            });
-            });
-    // console.log(allIngredients);
+    
+ function processrecipes(recipeArray){
+    var allIngredients = loadIngredients(recipeArray);
+    var compiledIngredients = compileIngredients(allIngredients);
+    
+    var listArray = concatIngred(compiledIngredients);
+        return listArray;
     };
     
-    function compileIngredients(){
+    function loadIngredients(recipeArray){
+        var returnArray = [];
+        recipeArray.forEach(function(cur){
+            var sliceArray = cur.slice(1);
+                        
+            sliceArray.forEach(function(cur){
+                returnArray.push(cur);
+
+            });
+            });
+return returnArray};
+    
+    function compileIngredients(someArray){
         var talliedIngredients = [];
+        var returnArray = [];
         
-        allIngredients.forEach(function(cur){
+        someArray.forEach(function(cur){
+            // console.log(cur);
             var thisIngredient = cur[2];
             var howMany = 0;
             var tempMatch = false;
             var pushArray = '';
             
             if(!(talliedIngredients.includes(thisIngredient))){
-                 allIngredients.forEach(function(cur){
+                 someArray.forEach(function(cur){
               tempMatch = cur.includes(thisIngredient);
                 if(tempMatch){
                     howMany++;
@@ -66,31 +158,31 @@ var dataController = (function(){
             });
                 pushArray = [(cur[0] * howMany), cur[1], cur[2]];
             
-            compiledIngredients.push(pushArray);
+            returnArray.push(pushArray);
             };
             
             
         });
         // console.log(talliedIngredients);
-        console.log(compiledIngredients);
+        // console.log(compiledIngredients);
+        return returnArray
     };
     
-    function concatIngred(){
-        compiledIngredients.forEach(function(cur){
+    function concatIngred(someArray){
+        var concatIngredList = [];
+        someArray.forEach(function(cur){
             var tempString = (cur[0] + cur[1] + cur[2]);
             concatIngredList.push(tempString);
         });
         // console.log(concatIngredList);
+        return concatIngredList;
     };
     
-    function processRecipes(){
-    loadIngredients();
-    compileIngredients();
-    concatIngred();
-    };
+
     
     return {
-       processRecipes: processRecipes
+       processrecipes: processrecipes,
+        recipes: recipes
     }
 })();
 
@@ -145,20 +237,62 @@ var UIController = (function(){
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 
-var appController = (function(dataCtrl, UICtrl){
+var appController = (function(calCtrl, dataCtrl, UICtrl){
     
     //// VARIABLES ////
-    
+    var monthRecipes = [];
     
     //// FUNCTIONS ////
     function innit(){
-        dataCtrl.processRecipes();
+        var breakfastArray = dataCtrl.processrecipes(dataCtrl.recipes.breakfast);
+        monthRecipes = calCtrl.generateMonth(2, dataCtrl.recipes);
+        weeklyShoppingList();
     }
+    
+    function weeklyShoppingList(){
+        var weeklyLists = [];
+        var breakfastOrder = [];
+        var lunchOrder = [];
+        var dinnerOrder = [];
+        monthRecipes.forEach(function(cur, index){
+            var ingredientList = [];
+            var shoppingList = '';
+            
+            cur.forEach(function(cur){
+                cur.forEach(function(cur){
+                    ingredientList.push(cur);
+            
+                });
+            });
+            
+            var shoppingList = dataCtrl.processrecipes(ingredientList);
+            console.log(ingredientList);
+            console.log(shoppingList);
+            
+            ingredientList = [];
+            shoppingList = '';
+            // console.log(shoppingList);
+            
+            cur[0].forEach(function(cur){
+                breakfastOrder.push(cur[0]);
+            });
+            
+            cur[1].forEach(function(cur){
+                lunchOrder.push(cur[0]);
+            });
+            
+            cur[2].forEach(function(cur){
+                dinnerOrder.push(cur[0]);
+            });
+            
+        });
+        // console.log(breakfastOrder);
+    };
     
     return {
        innit: innit
     }
-})(dataController, UIController);
+})(calendarController, dataController, UIController);
 
 appController.innit();
 
